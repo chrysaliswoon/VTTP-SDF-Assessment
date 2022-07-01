d8680ccca98d8d6737ad89f43d1e3dd3bbabad46
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-07-01 14:44:11 +0800

    Completed Task 2

--- task02/src/main/java/assessment/core/Client.java
+++ task02/src/main/java/assessment/core/Client.java
@@ -26,62 +26,72 @@ public class Client {
     private String name = "Chrysalis Woon Yong Xin";
     String email = "chrysaliswoon@gmail.com";
 
-
-
     public Client(String IP, Integer PORT) throws IOException {
         this.IP = IP;
         this.PORT = PORT;
     }
 
-    public void start() throws IOException{
+    public void start() throws IOException {
         try {
             clientSocket = new Socket(IP, PORT);
             System.out.println("Connected to " + IP + " on Port: " + PORT);
-    
+
             out = new DataOutputStream(clientSocket.getOutputStream());
             in = new DataInputStream(clientSocket.getInputStream());
             oos = new ObjectOutputStream(out);
             ois = new ObjectInputStream(in);
-    
-            //? Reads the response from the server
+
+            // ? Reads the response from the server
             String response = ois.readUTF();
-            
-            //? Splits the response into Request ID and List of Integers
+
+            // ? Splits the response into Request ID and List of Integers
             String segments[] = response.split(" ");
             String reqID = segments[0];
             String integerList = segments[segments.length - 1];
             String[] numbers = integerList.split(",");
-    
-            //? Calculate the average of the scores
+
+            // ? Calculate the average of the scores
             Stream<String> stringStream = Arrays.stream(numbers);
             IntStream intStream = stringStream.mapToInt(value -> Integer.parseInt(value));
             double score = intStream.average().getAsDouble();
             float averageScore = (float) score;
-            // System.out.println(response);
-    
-            System.out.println("Request ID: "+ reqID);
-            System.out.println("Average: "+ averageScore);
-    
-    
-            //? Write answer back to server
-            oos.writeUTF(reqID);
-            oos.writeUTF(name);
-            oos.writeUTF(email);
-            oos.writeFloat(averageScore);
-            
-        
+
+            System.out.println("Request ID: " + reqID);
+            System.out.println("Average: " + averageScore);
+
+            // ? Write answer back to server
+            writeUTF(reqID);
+            writeUTF(name);
+            writeUTF(email);
+            writeFloat(averageScore);
+
             oos.flush();
-            stopConnection();
-            
+
+            Boolean readBoolean = ois.readBoolean();
+            if (readBoolean == false) {
+                System.out.println("FAILED");
+                stopConnection();
+            } else {
+                System.out.println("SUCCESS");
+            }
+
         } catch (IOException ex) {
             ex.printStackTrace();
         }
 
     }
 
+    public void writeUTF(String str) throws IOException {
+        oos.writeUTF(str);
+    }
+
+    public void writeFloat(Float num) throws IOException {
+        oos.writeFloat(num);
+    }
+
     public void stopConnection() throws IOException {
-        ois.close();
-        oos.close();
+        out.close();
+        in.close();
         clientSocket.close();
     }
 }
