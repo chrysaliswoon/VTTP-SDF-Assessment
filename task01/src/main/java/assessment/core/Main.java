d601188c9cc6c236eb5dcb4eed8f4c317a792281
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-07-01 09:23:40 +0800

    Able to take 2 params from the command line

---
+++ task01/src/main/java/assessment/core/Main.java
@@ -0,0 +1,16 @@
+package assessment.core;
+
+
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        String csvFile;
+        String templateFile;
+
+        if (args.length > 0) {
+            csvFile = args[0];
+            templateFile = args[1];
+        }
+    }
+}
